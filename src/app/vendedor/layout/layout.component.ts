6a1104b9cb20d73be3a6d6b93e8be1da9ef69290
pixgel <pixgel13040@gmail.com>
2022-06-16 20:53:33 -0500

    crear subasta con diseño

--- src/app/vendedor/layout/layout.component.ts
+++ src/app/vendedor/layout/layout.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  toggle:boolean=false;
+  
   constructor() { }
 
+
   ngOnInit(): void {
+
   }
+
+  toggleSlideBar(){
+    this.toggle=!this.toggle
+  }
+
 
 }
